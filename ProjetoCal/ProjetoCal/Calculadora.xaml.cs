1afbf5baa9cf522682e6a6a457bac662fa90ae28
CamillyOliveira <102779635+CamillyOliveira@users.noreply.github.com>
2022-04-01 00:41:44 -0300

    Add files via upload

---
+++ ProjetoCal/ProjetoCal/Calculadora.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetoCal
+{
+    /// <summary>
+    /// Lógica interna para Calculadora.xaml
+    /// </summary>
+    public partial class Calculadora : Window
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void BotaoMenos_Click(object sender, RoutedEventArgs e)
+        {
+            float primeiroNumero = Convert.ToInt32(priNumero.Text);
+            float segundoNumero = Convert.ToInt32(segNumero.Text);
+            float resultado = primeiroNumero - segundoNumero;
+            TextResultado.Text = Convert.ToString(resultado);
+        }
+
+        private void BotaoDividir_Click(object sender, RoutedEventArgs e)
+        {
+            float primeiroNumero = Convert.ToInt32(priNumero.Text);
+            float segundoNumero = Convert.ToInt32(segNumero.Text);
+            float resultado = primeiroNumero / segundoNumero;
+            TextResultado.Text = Convert.ToString(resultado);
+        }
+
+        private void BotaoSomar_Click(object sender, RoutedEventArgs e)
+        {
+            float primeiroNumero = Convert.ToInt32(priNumero.Text);
+            float segundoNumero = Convert.ToInt32(segNumero.Text);
+            float resultado = primeiroNumero + segundoNumero;
+            TextResultado.Text = Convert.ToString(resultado);
+        }
+
+        private void BotaoVezes_Click(object sender, RoutedEventArgs e)
+        {
+            float primeiroNumero = Convert.ToInt32(priNumero.Text);
+            float segundoNumero = Convert.ToInt32(segNumero.Text);
+            float resultado = primeiroNumero * segundoNumero;
+            TextResultado.Text = Convert.ToString(resultado);
+        }
+    }
+}
---
+++ ProjetoCal/ProjetoCal/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetoCal
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BotaoLogar_Click(object sender, RoutedEventArgs e)
+        {
+            string senha = PassSenha.Password.ToString();
+            string usuario = TextUsuario.Text;
+
+            if(usuario == "camillysouza" & senha == "12101996")
+            {
+                MessageBox.Show("LOGIN EFETUADO!!!");
+                TextUsuario.Clear();
+                PassSenha.Clear();
+                Calculadora calculadora = new Calculadora();
+                calculadora.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Usuário ou senha Inválido!!!");
+                TextUsuario.Clear();
+                PassSenha.Clear();
+            }
+        }
+    }
+}
